de609a217b8ff59838b578652c46c8637e4c86e6
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2022-11-12 19:29:02 +0100

    header style

--- index.php
+++ index.php
@@ -1,10 +1,36 @@
-<?php
+<?php include 'parts/header.php'; ?>
 
-include 'parts/header.php';
+<div class="content">
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit error impedit exercitationem soluta perferendis, voluptatum id! Dolores rem, tempora voluptatum dolorum nihil porro, delectus inventore corrupti necessitatibus reiciendis numquam quae!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat culpa dolore facilis suscipit, tempora impedit, error dicta assumenda voluptas repellat doloremque maxime eum porro ipsum nemo voluptate temporibus. Odio.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam voluptatem blanditiis deserunt cum, velit est itaque rem id ab perspiciatis molestias corrupti quasi natus. Quia tempore nobis nemo quas obcaecati.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deleniti reiciendis fuga ducimus nam? Quidem quam totam et debitis, nisi nihil esse suscipit illum, velit doloremque quae eveniet fuga quod.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit error impedit exercitationem soluta perferendis, voluptatum id! Dolores rem, tempora voluptatum dolorum nihil porro, delectus inventore corrupti necessitatibus reiciendis numquam quae!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat culpa dolore facilis suscipit, tempora impedit, error dicta assumenda voluptas repellat doloremque maxime eum porro ipsum nemo voluptate temporibus. Odio.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam voluptatem blanditiis deserunt cum, velit est itaque rem id ab perspiciatis molestias corrupti quasi natus. Quia tempore nobis nemo quas obcaecati.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deleniti reiciendis fuga ducimus nam? Quidem quam totam et debitis, nisi nihil esse suscipit illum, velit doloremque quae eveniet fuga quod.</p>
+</div>
 
+<div class="content">
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit error impedit exercitationem soluta perferendis, voluptatum id! Dolores rem, tempora voluptatum dolorum nihil porro, delectus inventore corrupti necessitatibus reiciendis numquam quae!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat culpa dolore facilis suscipit, tempora impedit, error dicta assumenda voluptas repellat doloremque maxime eum porro ipsum nemo voluptate temporibus. Odio.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam voluptatem blanditiis deserunt cum, velit est itaque rem id ab perspiciatis molestias corrupti quasi natus. Quia tempore nobis nemo quas obcaecati.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deleniti reiciendis fuga ducimus nam? Quidem quam totam et debitis, nisi nihil esse suscipit illum, velit doloremque quae eveniet fuga quod.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit error impedit exercitationem soluta perferendis, voluptatum id! Dolores rem, tempora voluptatum dolorum nihil porro, delectus inventore corrupti necessitatibus reiciendis numquam quae!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat culpa dolore facilis suscipit, tempora impedit, error dicta assumenda voluptas repellat doloremque maxime eum porro ipsum nemo voluptate temporibus. Odio.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam voluptatem blanditiis deserunt cum, velit est itaque rem id ab perspiciatis molestias corrupti quasi natus. Quia tempore nobis nemo quas obcaecati.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deleniti reiciendis fuga ducimus nam? Quidem quam totam et debitis, nisi nihil esse suscipit illum, velit doloremque quae eveniet fuga quod.</p>
+</div>
 
+<div class="content">
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit error impedit exercitationem soluta perferendis, voluptatum id! Dolores rem, tempora voluptatum dolorum nihil porro, delectus inventore corrupti necessitatibus reiciendis numquam quae!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat culpa dolore facilis suscipit, tempora impedit, error dicta assumenda voluptas repellat doloremque maxime eum porro ipsum nemo voluptate temporibus. Odio.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam voluptatem blanditiis deserunt cum, velit est itaque rem id ab perspiciatis molestias corrupti quasi natus. Quia tempore nobis nemo quas obcaecati.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deleniti reiciendis fuga ducimus nam? Quidem quam totam et debitis, nisi nihil esse suscipit illum, velit doloremque quae eveniet fuga quod.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit error impedit exercitationem soluta perferendis, voluptatum id! Dolores rem, tempora voluptatum dolorum nihil porro, delectus inventore corrupti necessitatibus reiciendis numquam quae!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fugiat culpa dolore facilis suscipit, tempora impedit, error dicta assumenda voluptas repellat doloremque maxime eum porro ipsum nemo voluptate temporibus. Odio.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam voluptatem blanditiis deserunt cum, velit est itaque rem id ab perspiciatis molestias corrupti quasi natus. Quia tempore nobis nemo quas obcaecati.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deleniti reiciendis fuga ducimus nam? Quidem quam totam et debitis, nisi nihil esse suscipit illum, velit doloremque quae eveniet fuga quod.</p>
+</div>
 
-
-
-
-include 'parts/footer.php';
+<?php include 'parts/footer.php'; ?>
